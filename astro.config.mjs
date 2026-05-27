f822b06c7759c7fa5b70697b5e996c1a15e0f011
okadake <okadake@users.noreply.github.com>
2026-05-28 07:04:41 +0900

    ルートパス変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://okadake.github.io',
-  base: '/ednaya',
   vite: {
     plugins: [tailwindcss()]
   }
