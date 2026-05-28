897f31bcb8f45de781f8fff0eadfa59c714a1c89
okadake <okadake@users.noreply.github.com>
2026-05-28 12:03:49 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://okadake.github.io',
+  site: 'https://edna.jp',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
