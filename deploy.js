438103e352b5eea11c9790248403f757f9dd8c30
okadake <okadake@users.noreply.github.com>
2026-06-04 15:49:33 +0900

    最新

---
+++ deploy.js
@@ -0,0 +1,35 @@
+import FtpDeploy from 'ftp-deploy';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const ftpDeploy = new FtpDeploy();
+
+const config = {
+  user: process.env.FTP_USER,
+  password: process.env.FTP_PASSWORD,
+  host: 'ftp.lolipop.jp',
+  port: 21,
+  localRoot: './dist',
+  remoteRoot: '/edna2026/ednaya/',
+  include: ['**/*'],
+  exclude: ['images/**/*', '.DS_Store', 'node_modules/**/*'],
+  deleteRemote: false,
+  forcePasv: true,
+  sftp: false,
+};
+
+console.log('Starting deployment (images excluded)...');
+
+ftpDeploy
+  .deploy(config)
+  .then((res) => {
+    console.log('✓ Deployment successful!');
+    console.log('Deployed files:', res.length);
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error('✗ Deployment failed:');
+    console.error(err);
+    process.exit(1);
+  });
